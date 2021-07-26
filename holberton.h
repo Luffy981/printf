e94b780edd2fc71ea07227e5f1e759b1aa3770d0
luffy981 <jhonsmith981@hotmail.com>
2021-07-26 08:25:54 +0000

    holberton.h

--- holberton.h
+++ holberton.h
@@ -1,12 +1,18 @@
 #ifndef _PRINTF_
 #define _PRINTF_
-typedef struct op
-{
-	char *op;
-	void *f;
-}op_t;
-int _printf(const char *format, ...);
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+typedef struct op
+{
+	char *op;
+	int (*f)(va_list);
+} op_t;
+int _printf(const char *format, ...);
+int d_puts(char c);
+int p_char(va_list);
+int (*check_match(const char *format))(va_list);
+int p_string(va_list);
+int _strlen(const char *str);
 #endif
