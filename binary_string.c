ccf790f410b662a1540633244d38e36ec8c3c7c5
luffy981 <jhonsmith981@hotmail.com>
2021-07-30 07:58:37 +0000

    Binary fixed and hex fixed

---
+++ binary_string.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+int binary_string_to_buff(char **bin, char **ffub, int size_string)
+{
+	int count = 0;
+
+	while (size_string >= 0)
+	{
+		**ffub = **bin+'0';
+		(*bin)--;
+		(*ffub)++;
+		count++;
+		size_string--;
+	}
+	return (count);
+}
---
+++ hex_printf.c
@@ -0,0 +1,96 @@
+#include "holberton.h"
+/**
+ * p_heX_string - function to print a string with hexadecimal characaters
+ * @S: Argument
+ * @ffub: Double pointer to buffer
+ *
+ * Return: Return count
+ */
+int p_heX_string(va_list S, char **ffub)
+{
+	char *str;
+	int count = 0;
+	char **rts = &str;
+	char r[] = "\\x";
+	char *p = r;
+
+	str = va_arg(S, char*);
+	if (str == NULL)
+		str = "(null)";
+	while (**rts != '\0')
+	{
+		if ((**rts > 0 && **rts < 32) || **rts >= 127)
+		{
+			**ffub = *p;
+			(*ffub)++;
+			count++;
+			p++;
+			**ffub = *p;
+			(*ffub)++;
+			p--;
+			count++;
+			count += number_to_hex(**rts, ffub);
+			(*rts)++;
+		}
+		**ffub = **rts;
+		(*ffub)++;
+		(*rts)++;
+		count++;
+	}
+	return (count);
+}
+/**
+ * number_to_hex - function to convert number to heXadecimal
+ * @n: Argument
+ * @ffub: Double pointer to buff
+ *
+ * Return: Return count
+ */
+int number_to_hex(long int n, char **ffub)
+{
+	char hex[] = "0123456789ABCDEF";
+	char Xeh[50];
+	int a = 0;
+	long int t;
+	char *x = Xeh;
+	int count = 0;
+
+	while (a <= 50)
+		Xeh[a] = '\0', a++;
+	while (n != 0)
+	{
+		t = n % 16;
+		*x = hex[t];
+		count++;
+		n /= 16;
+		x++;
+	}
+	x--;
+	count--;
+	return (string_to_buff(&x, ffub, count));
+}
+/**
+ * p_heX_number - function to convert a number to hexadecimal
+ * @X: Argument
+ * @ffub: Double pointer to buffer
+ *
+ * Return: Return count
+ */
+int p_heX_number(va_list X, char **ffub)
+{
+	int n;
+	long int num;
+	int count = 0;
+
+	n = va_arg(X, int);
+	if (n < 0)
+	{
+		num = -n;
+		num = 4294967295 - num + 1;
+	} else if (n >= 0)
+	{
+		num = n;
+	}
+	count += number_to_hex(num, ffub);
+	return (count);
+	}
